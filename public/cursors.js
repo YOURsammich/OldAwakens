05281cbb746a6b289cd13192a51bd9b8037157df
Sammich <oel333@yahoo.com>
2017-02-16 23:24:03 -0600

    :+1:

--- public/cursors.js
+++ public/cursors.js
@@ -30,7 +30,7 @@ function changeCursor(id, newCursor) {
     document.addEventListener('mousemove', function (e) {
         var x = e.clientX / window.innerWidth
         var y = e.clientY / window.innerHeight
-        if (moves == 0 && Attributes.get('cursors')) {
+        if (moves == 0 && Attributes.get('toggle-cursors')) {
             socket.emit('updateCursor', {
                 x : x,
                 y : y
